4d1417e7aa0a2960bef78c131153b1b515facbee
dgzornoza <dgzornoza@dgzornoza.com>
2021-03-14 22:33:44 +0100

    correccion ruta base del ensamblado

--- src/OpenVpnAuthUser/Program.cs
+++ src/OpenVpnAuthUser/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyModel;
 using Microsoft.Extensions.Hosting;
 using OpenVpnAuthUser.Models;
 using OpenVpnAuthUser.Services;
 using Serilog;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -16,16 +16,20 @@ namespace OpenVpnAuthUser
         /// <summary>
         /// Application configuration
         /// </summary>
-        static public IConfiguration Configuration { get; } = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environments.Production}.json", optional: true)
-            .AddEnvironmentVariables()
-            .Build();
+        static public IConfiguration Configuration { get; private set; }
 
 
         static async Task Main(string[] args)
         {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? Environments.Production}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(Configuration)
                 .Enrich.FromLogContext()
@@ -34,6 +38,12 @@ namespace OpenVpnAuthUser
             try
             {                
                 Log.Information("App started");
+
+
+
+                LogUserName();
+
+
 
                 ServiceCollection serviceCollection = new ServiceCollection();
                 ConfigureServices(serviceCollection);
@@ -66,6 +76,20 @@ namespace OpenVpnAuthUser
         }
 
 
+        private static void LogUserName()
+        {
+            string userName = Environment.UserName;
+
+
+            Log.Information($"UserName: {userName}");
+
+            Log.Information($"Launched from {Environment.CurrentDirectory}");
+            Log.Information($"Physical location {AppDomain.CurrentDomain.BaseDirectory}");
+            Log.Information($"AppContext.BaseDir {AppContext.BaseDirectory}");
+            Log.Information($"Runtime Call {Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)}");
+
+        }
+    
 
 
         private static void ConfigureServices(IServiceCollection services)
